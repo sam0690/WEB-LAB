a248407f4194d28aadb353f50f520260ed190605
sam0690 <118650996+sam0690@users.noreply.github.com>
2025-01-26 22:18:53 +0545

    web

--- Lab-1/script.js
+++ Lab-1/script.js
@@ -5,4 +5,20 @@ const formValidator = (e) => {
         alert("username should be at least 4 characters");
         return false;
     }
+
+    const password = document.getElementsByClassName('password');
+    if( ! password.value.match(/[0-9][a-zA-Z0-9]*[a-zA-Z0-9][$]/)){
+        password.style.border = 'soid 1px red';
+        alert("Password should start with a digit and end with $.");
+        return false;
+    }
+
+    const boxes = document.getElementsByName('Bsc.CSIT');
+    for(leti=0; i<boxes.length; i++){
+        if( ! boxes[i].checked){
+            alert("Please Select Your educational field.");
+            return false;
+        }
+    }
+    return true;
 }
