88236acb3b66e5eb1b0aa7f1e75b973e307ae6e2
sam0690 <118650996+sam0690@users.noreply.github.com>
2025-02-04 15:01:18 +0545

    web

--- Lab-1/script.js
+++ Lab-1/script.js
@@ -1,26 +1,41 @@
 function formValidator(){
-    console.log("KHAI TA CHALEKO ???")
-    const username = document.getElementsByClassName('username');
-    if(username.value.trim().length < 4){
-        console.log("MILYO TA ?????")
-        username.style.border = "solid 1px red";
+    console.log("validation started")
+    //validating username or getting the username correctly
+    const username = document.getElementById('username').value;
+    if(username.length < 4){
+        console.log("username validation failes")
+        document.getElementById('username').style.border = "solid 1px red";
         alert("username should be at least 4 characters");
         return false;
     }
 
-    const password = document.getElementsByClassName('password');
-    if( ! password.value.match(/[0-9][a-zA-Z0-9]*[a-zA-Z0-9][$]/)){
-        password.style.border = 'soid 1px red';
-        alert("Password should start with a digit and end with $.");
+    //getting the correct pattern for a password
+
+    const password = document.getElementById('password').value;
+    const passpattern = /^[0-9].*#$/;
+    if( !passpattern.test(password)){
+        document.getElementById('password').style.border = 'soid 1px red';
+        alert("Password should start with a digit and end with #.");
         return false;
     }
 
-    const box = document.getElementsByName('Bsc.CSIT');
-    for(leti=0; i<box.length; i++){
-        if( ! box[i].checked){
-            alert("Please Select Your educational field.");
-            return false;
+
+    //validating checkbox selection
+
+    const education = document.getElementsByName('education[]');
+    let checked = false;
+    for ( let i = 0 ; i<education.length;i++){
+        if(education[i].checked){
+            checked = true;
+            break;
         }
     }
+
+    if(!checked){
+        alert("Please select at least one educational field");
+        return false;
+    }
+
+
     return true;
 }
